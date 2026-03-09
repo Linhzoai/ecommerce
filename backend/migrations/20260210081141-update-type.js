f7d3948d18cb5d09be0c3570a4e376e67d0cb197
Linh Nguyen <linhbarao@gmail.com>
2026-03-09 17:29:56 +0700

    thr commit

--- backend/migrations/20260210081141-update-type.js
+++ backend/migrations/20260210081141-update-type.js
@@ -2,14 +2,14 @@
 /** @type {import('sequelize-cli').Migration} */
 export default {
   async up (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("products", "price", {
+    await queryInterface.changeColumn("Products", "price", {
       type: Sequelize.DECIMAL(10, 2),
       allowNull: false,
     });
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.changeColumn("products", "price", {
+    await queryInterface.changeColumn("Products", "price", {
       type: Sequelize.DECIMAL(10, 2),
       allowNull: false,
     });
