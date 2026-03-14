68141621a1afb680b7f0f865bd6f1ad6b96e901f
Linh Nguyen <linhbarao@gmail.com>
2026-03-14 16:15:43 +0700

    add ssl

--- backend/config/config.cjs
+++ backend/config/config.cjs
@@ -22,5 +22,12 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "postgres",
+    port: 5432,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
